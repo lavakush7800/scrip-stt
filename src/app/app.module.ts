2c51148357d32f23974d37150720911c66854b11
lavakush7800 <lavakushgiri@gmail.com>
2021-10-08 19:35:14 +0530

    generate header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DemoComponent } from './demo/demo.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DemoComponent
+    DemoComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
