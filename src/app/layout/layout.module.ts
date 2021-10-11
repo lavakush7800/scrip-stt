517eadfa5d6469be908bd3bb5b2a23f561d3f89e
lavakush7800 <lavakushgiri@gmail.com>
2021-10-11 19:21:55 +0530

    modify code

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    SidebarComponent
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
